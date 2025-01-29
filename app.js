762bd786647546e605df501a5ca99af10240540e
Tiraz <hugo.suterio@hotmail.com>
2025-01-28 23:04:28 -0300

    Coletando nome, mostrando na tela e limpando campo de input.

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+listaAmigos = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+function adicionarAmigo(){
+    let amigo = document.getElementById("amigo").value;//pega o nome digitado 
+    if(amigo == ""){//Compara se o valor coletado está vazio 
+        alert("Digite um nome valido!");
+    }else{
+        listaAmigos.push(amigo);
+        let lista = document.getElementById("listaAmigos");
+        let nome = document.createElement("li");
+        nome.textContent = amigo;
+        lista.appendChild(nome);
+        let limpar = document.querySelector("input");
+        limpar.value = "";
+    }
+}
